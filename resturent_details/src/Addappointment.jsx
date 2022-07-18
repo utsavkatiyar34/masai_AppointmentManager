00e537e321f07f1844ce4938b3c253b5a6cfddff
utsavkatiyar34 <utsavkatiyar34@gmail.com>
2022-07-19 02:22:09 +0530

    adding done

--- resturent_details/src/Addappointment.jsx
+++ resturent_details/src/Addappointment.jsx
@@ -1,16 +1,80 @@
-import React from 'react'
-import './addappointment.css'
+import React, { useState, useEffect } from "react";
+import "./addappointment.css";
+import axios from "axios";
+import { v4 } from "uuid";
 
 export default function Addappointment() {
+  // const [click, setClick]=useState(0);
+  // let handleClick=()=>{
+  //         setClick(1);
+  // }
+  const [data, setData] = useState({
+    name: "",
+    age: "",
+    gender: "",
+    details: "",
+    time: "",
+    date: "",
+  });
+  let handleInput = (val) => {
+    const key = val.target.name;
+    const value = val.target.value;
+    setData({ ...data, [key]: value });
+  };
+  let handleClick = () => {
+    axios.post('http://localhost:3006/appointments', {...data})
+    .then(function (response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
   return (
-    <div className='addform'>
-        <input type="text" placeholder='Enter Name'></input>
-        <input type="text" placeholder='Enter Age'></input>
-        <input type="text" placeholder='Enter Gender'></input>
-        <input type="text" placeholder='Enter Appointment Details'></input>
-        <input type="time" placeholder='Enter Time'></input>
-        <input type="date" placeholder='Enter Date'></input>
-        <button >Add Appointment</button>
+    <div className="addform">
+      <input
+        name="name"
+        onChange={handleInput}
+        value={data.name}
+        type="text"
+        placeholder="Enter Name"
+      ></input>
+      <input
+        name="age"
+        onChange={handleInput}
+        value={data.age}
+        type="number"
+        placeholder="Enter Age (eg. 1,2,3...)"
+      ></input>
+      <input
+        name="gender"
+        onChange={handleInput}
+        value={data.gender}
+        type="text"
+        placeholder="Enter Gender"
+      ></input>
+      <input
+        name="details"
+        onChange={handleInput}
+        value={data.details}
+        type="text"
+        placeholder="Enter Appointment Details"
+      ></input>
+      <input
+        name="time"
+        onChange={handleInput}
+        value={data.time}
+        type="time"
+        placeholder="Enter Time"
+      ></input>
+      <input
+        name="date"
+        onChange={handleInput}
+        value={data.date}
+        type="date"
+        placeholder="Enter Date"
+      ></input>
+      <button onClick={handleClick}>Add Appointment</button>
     </div>
-  )
+  );
 }
--- resturent_details/src/App.js
+++ resturent_details/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from'./Login';
+import Addappointment from './Addappointment';
 
 function App() {
   return (
-    <div className="App">
-     <Login/>
+    <div>
+     {/* <Login/> */}
+     <Addappointment/>
     </div>
   );
 }
